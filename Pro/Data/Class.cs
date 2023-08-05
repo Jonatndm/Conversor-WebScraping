bd8a19abe8b00ac0e69f1865c202f3500581853d
Michael Del Rosario <Michael Del Rosario@LAPTOP-MICHAEL>
2023-08-04 21:08:08 -0400

    Add project files.

---
+++ Pro/Data/Class.cs
@@ -0,0 +1,22 @@
+﻿using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+using System.Linq;
+
+namespace Pro.Data
+{
+	public class Class
+	{
+		public HtmlWeb web;
+		public HtmlDocument doc;
+		public HtmlNode body;
+
+		public Class(string url)
+		{
+			this.web = new HtmlWeb();
+			this.doc = web.Load(url);
+			this.body = doc.DocumentNode.CssSelect(".YMlKec.fxKbKc").First();
+			
+		}
+
+	}
+}
